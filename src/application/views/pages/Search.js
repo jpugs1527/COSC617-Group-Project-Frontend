e6dfe10a8a6eab940d82d07f8fa0cdcf9b3f99a4
Jayson Malabanan <jmalab1@students.towson.edu>
2020-03-21 21:43:38 -0400

    loops through json array for search view

--- src/application/views/pages/Search.js
+++ src/application/views/pages/Search.js
@@ -5,10 +5,33 @@ import { connect } from 'react-redux'
 import Header from '../common/Header'
 import Footer from '../common/Footer'
 import Search from '../common/Search'
+import customData from '../assets/json/sample-cars';
 
 class SearchPage extends Component {
+
     render() { 
         const image = require('../assets/images/car.jpg');
+        let cards = [];
+
+        customData.data.map((data, idx) => (
+            cards.push(
+                <Card
+                    border="dark"
+                    key={idx}>
+                    <Card.Img variant="top" src={image}/>
+                    <Card.Body>
+                    <Card.Title>{data.year} {data.manufacturer} {data.model}</Card.Title>
+                    <Card.Text>
+                        Some quick example text to build on the card title and make up the
+                        bulk of the card's content.
+                        <br/><br/>
+                        <a href="#">View</a>
+                    </Card.Text>
+                    </Card.Body>
+                </Card>
+            )
+          ));
+
         return (
             <div>
                 <Header />
@@ -18,56 +41,7 @@ class SearchPage extends Component {
                 <Container><br/>
                     <Search/><br/>
                     <CardColumns>
-                        <Card border="dark" className="carListCards">
-                            <Card.Img variant="top" src={image}/>
-                            <Card.Body>
-                            <Card.Title>Dark Card Title</Card.Title>
-                            <Card.Text>
-                                Some quick example text to build on the card title and make up the bulk
-                                of the card's content.
-                            </Card.Text>
-                            </Card.Body>
-                        </Card>
-                        <Card border="dark" className="carListCards">
-                            <Card.Img variant="top" src={image}/>
-                            <Card.Body>
-                            <Card.Title>Dark Card Title</Card.Title>
-                            <Card.Text>
-                                Some quick example text to build on the card title and make up the bulk
-                                of the card's content.
-                            </Card.Text>
-                            </Card.Body>
-                        </Card>
-                        <Card border="dark" className="carListCards">
-                            <Card.Img variant="top" src={image}/>
-                            <Card.Body>
-                            <Card.Title>Dark Card Title</Card.Title>
-                            <Card.Text>
-                                Some quick example text to build on the card title and make up the bulk
-                                of the card's content.
-                            </Card.Text>
-                            </Card.Body>
-                        </Card>
-                        <Card border="dark" className="carListCards">
-                            <Card.Img variant="top" src={image}/>
-                            <Card.Body>
-                            <Card.Title>Dark Card Title</Card.Title>
-                            <Card.Text>
-                                Some quick example text to build on the card title and make up the bulk
-                                of the card's content.
-                            </Card.Text>
-                            </Card.Body>
-                        </Card>
-                        <Card border="dark" className="carListCards">
-                            <Card.Img variant="top" src={image}/>
-                            <Card.Body>
-                            <Card.Title>Dark Card Title</Card.Title>
-                            <Card.Text>
-                                Some quick example text to build on the card title and make up the bulk
-                                of the card's content.
-                            </Card.Text>
-                            </Card.Body>
-                        </Card>
+                        {cards}
                     </CardColumns>
                 </Container>
                 <Footer />
