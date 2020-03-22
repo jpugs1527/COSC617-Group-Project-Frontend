92916838de74be40bc94fd261b1fc8435c6f40c9
Jayson Malabanan <jmalab1@students.towson.edu>
2020-03-21 20:19:32 -0400

    added cards for car view

--- src/application/views/pages/Search.js
+++ src/application/views/pages/Search.js
@@ -1,23 +1,74 @@
 import React, { Component } from 'react'
-import { Card, Container, Col, Row, Form, FormControl, Button} from 'react-bootstrap'
+import { Card, Container, CardColumns } from 'react-bootstrap'
 import { Helmet } from "react-helmet"
 import { connect } from 'react-redux'
 import Header from '../common/Header'
 import Footer from '../common/Footer'
 import Search from '../common/Search'
 
-class CarsListViewPage extends Component {
-
+class SearchPage extends Component {
     render() { 
+        const image = require('../assets/images/car.jpg');
         return (
             <div>
                 <Header />
                 <Helmet>
                     <title>Available Cars</title>
                 </Helmet>
-                <Container>
-                    <br/>
-                    <Search/>
+                <Container><br/>
+                    <Search/><br/>
+                    <CardColumns>
+                        <Card border="dark" className="carListCards">
+                            <Card.Img variant="top" src={image}/>
+                            <Card.Body>
+                            <Card.Title>Dark Card Title</Card.Title>
+                            <Card.Text>
+                                Some quick example text to build on the card title and make up the bulk
+                                of the card's content.
+                            </Card.Text>
+                            </Card.Body>
+                        </Card>
+                        <Card border="dark" className="carListCards">
+                            <Card.Img variant="top" src={image}/>
+                            <Card.Body>
+                            <Card.Title>Dark Card Title</Card.Title>
+                            <Card.Text>
+                                Some quick example text to build on the card title and make up the bulk
+                                of the card's content.
+                            </Card.Text>
+                            </Card.Body>
+                        </Card>
+                        <Card border="dark" className="carListCards">
+                            <Card.Img variant="top" src={image}/>
+                            <Card.Body>
+                            <Card.Title>Dark Card Title</Card.Title>
+                            <Card.Text>
+                                Some quick example text to build on the card title and make up the bulk
+                                of the card's content.
+                            </Card.Text>
+                            </Card.Body>
+                        </Card>
+                        <Card border="dark" className="carListCards">
+                            <Card.Img variant="top" src={image}/>
+                            <Card.Body>
+                            <Card.Title>Dark Card Title</Card.Title>
+                            <Card.Text>
+                                Some quick example text to build on the card title and make up the bulk
+                                of the card's content.
+                            </Card.Text>
+                            </Card.Body>
+                        </Card>
+                        <Card border="dark" className="carListCards">
+                            <Card.Img variant="top" src={image}/>
+                            <Card.Body>
+                            <Card.Title>Dark Card Title</Card.Title>
+                            <Card.Text>
+                                Some quick example text to build on the card title and make up the bulk
+                                of the card's content.
+                            </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </CardColumns>
                 </Container>
                 <Footer />
             </div>
@@ -25,4 +76,4 @@ class CarsListViewPage extends Component {
     }
 }
 
-export default connect(null)(CarsListViewPage)
+export default connect(null)(SearchPage)
